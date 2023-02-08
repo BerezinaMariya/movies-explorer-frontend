b8e38d6a4bb10a61844a27392399252a938abee2
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-02-08 21:26:20 +0300

    fix: when switch to protected rout redirect to /

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,7 @@ const ProtectedRoute = ({ component: Component, ...props }) => {
   return (
     <Route>
       {() =>
-        props.loggedIn ? <Component {...props} /> : <Redirect to="./signin" />
+        props.loggedIn ? <Component {...props} /> : <Redirect to="./" />
       }
     </Route>
   );
